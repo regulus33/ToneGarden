492020bd8f05e3c1da1b2451204c35a46dbb0e83
zachary <zachary.rowden@showmax.com>
2021-05-08 23:23:35 +0200

    Adding accordian for more features

--- app/javascript/bundles/Styles/StylesPresetShowScreen.ts
+++ app/javascript/bundles/Styles/StylesPresetShowScreen.ts
@@ -8,41 +8,40 @@ interface Props {
 
 const useStyles = makeStyles({
     saveButton: (props: Props) => ({
-       backgroundColor: props.dominantColor,
+        backgroundColor: props.dominantColor,
         color: 'white',
         '&:hover': {
             background: props.secondaryColor,
         }
     }),
-    largeSlider: {
-        marginTop: '14px',
-        padding: '8px'
+    headerContainer: {
+        padding: '16px 32px'
     },
-   presetFormCard: {
-       padding: '32px 16px',
-       maxHeight: 'calc(100vh - 300px)',
-       minWidth: '500px',
-       margin: '0 auto',
-       [`@media (max-width: ${Breakpoints.mobile})`]: {
-           minWidth: 0,
-           width: 'calc(100vw - 32px)'
-       }
+    presetFormCard: {
+        height: 'fit-content',
+        minWidth: '500px',
+        margin: '0 auto',
+        [`@media (max-width: ${Breakpoints.mobile})`]: {
+            minWidth: 0,
+            width: 'calc(100vw - 32px)'
+        }
     },
     pitchSliderContainer: {
         display: 'flex',
-        justifyContent: 'space-around'
+        justifyContent: 'space-around',
+        padding: '24px 0'
     },
     audioControlsContainer: {
-       marginTop: '32px',
-       display: 'flex',
+        marginTop: '32px',
+        display: 'flex',
+        padding: '0 15px 0 17px',
         justifyContent: 'space-between',
         '& svg': {
             transform: 'scale(130%)',
-            // color: Colors.defaultBlue
         }
     },
     saveButtonContainer: {
-       marginRight: '22px'
+        marginRight: '22px'
     }
 })
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # MUST BE 40hz from eachother and under 1500hz subtract small from large and you get brat frequency
-Preset.create(name: 'Alertness', left: 700, right: 720)
-Preset.create(name: 'Dreaming', left: 800, right: 807)
-Preset.create(name: 'Peace', left: 766.15, right: 766.15 + 30)
-Preset.create(name: 'Sleep', left: 342.24, right: 342.24 + 3)
-Preset.create(name: 'Focus', left: 600.24, right: 600.24 + 12)
+Preset.create(name: 'Alertness', left: 700, right: 720, editable: false)
+Preset.create(name: 'Dreaming', left: 800, right: 807, editable: false)
+Preset.create(name: 'Peace', left: 766.15, right: 766.15 + 30, editable: false)
+Preset.create(name: 'Sleep', left: 342.24, right: 342.24 + 3, editable: false)
+Preset.create(name: 'Focus', left: 600.24, right: 600.24 + 12, editable: false)
