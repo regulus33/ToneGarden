27ff0ddcee2a399a03ce4f77c824fdc6f8157776
zachary <zachary.rowden@showmax.com>
2021-08-03 08:18:22 +0200

    db

--- app/models/binaural_beat.rb
+++ app/models/binaural_beat.rb
@@ -3,8 +3,6 @@
 # Binaural beat data model
 class BinauralBeat < ApplicationRecord
 
-  validate :noiseLevelJavascriptNumber
-
   def editable?
     editable.positive?
   end
@@ -13,13 +11,5 @@ class BinauralBeat < ApplicationRecord
     return "#{name} (copy)" unless editable?
 
     logger.error(LoggerService.log_args('Called copy_name on editable beat!', { beat: self.inspect }))
-  end
-
-  def noiseLevelJavascriptNumber
-    return if noiseLevel == '-Infinity'
-
-    unless noiseLevel.match(/^\d*\.?\d*$/)
-      errors.add('Bad data given to noiseLevel')
-    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_01_191006) do
+ActiveRecord::Schema.define(version: 2021_08_01_201601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,13 +19,13 @@ ActiveRecord::Schema.define(version: 2021_08_01_191006) do
     t.float "beatOscillator"
     t.float "carrierOscillator"
     t.float "volume"
-    t.string "noiseLevel"
     t.float "editable"
     t.string "name"
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "description", default: "user preset"
+    t.integer "noiseLevel"
     t.index ["user_id"], name: "index_binaural_beats_on_user_id"
   end
 
