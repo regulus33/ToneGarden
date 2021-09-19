cc6ab0297358de849c65e96845d80c5b784a3867
zachary <zachary.rowden@showmax.com>
2021-09-19 13:35:38 +0200

    Whitelist new user columns in serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 # Exposes correct attributes of User
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :email, :use_white_noise, :use_audio_worklet
+  attributes :email, :use_white_noise, :use_audio_worklet, :use_white_noise, :use_audio_worklet
 end
