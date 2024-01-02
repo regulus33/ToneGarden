c25b0d5d1be87793809a1b2011375e5be6acb1b1
Zachary Rowden <zacharyrowden89@gmail.com>
2024-01-02 18:47:28 +0100

    Make non editables not editable

--- Gemfile
+++ Gemfile
@@ -65,3 +65,5 @@ gem "webpacker", "~> 5.2"
 
 gem "react_on_rails", "~> 12.0"
 gem 'mini_racer', platforms: :ruby
+
+gem 'rubocop-rails'
--- app/models/binaural_beat.rb
+++ app/models/binaural_beat.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
+# Binaural beat data model
 class BinauralBeat < ApplicationRecord
+  def editable?
+    editable.positive?
+  end
+
+  def copy_name
+    return "#{name} (copy)" unless editable?
+
+    logger.error(LoggerService.log_args('Called copy_name on editable beat!', { beat: self.inspect }))
+  end
 end
